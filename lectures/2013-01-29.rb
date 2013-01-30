0855d40d3b07cc20e7cb72aa68240abe34e3707d
Ron Jones <rrrron@gmail.com>
2013-01-29 22:01:27 -0500

    Daily exercises finished

--- lectures/2013-01-29.rb
+++ lectures/2013-01-29.rb
@@ -1,66 +1,66 @@
-# print "What is your first name? "
-# first = gets.chomp.rstrip
-# puts "Your first name is #{first}."
-# print "What is your last name?"
-# last = gets.chomp.rstrip
-# puts  "Your last name is #{last}."
+print "What is your first name? "
+first = gets.chomp.rstrip
+puts "Your first name is #{first}."
+print "What is your last name?"
+last = gets.chomp.rstrip
+puts  "Your last name is #{last}."
 
-# full_name = first + " " + last
+full_name = "#{first} #{last}}"
 
-# puts  "Your full name is #{full_name}"
+puts  "Your full name is #{full_name}"
 
-# puts "How old are you?"
-# age = gets.chomp.to_i
+puts "How old are you?"
+age = gets.chomp.to_i
 
-# if age < 21
-#   puts "You are too young to drink!"
-#   canDrink = false
-# else
-#   puts "Have a beer."
-#   canDrink = true
-# end
+if age < 21
+  puts "You are too young to drink!"
+  canDrink = false
+else
+  puts "Have a beer."
+  canDrink = true
+end
 
-# print "What is your address?"
-# address = gets.chomp
+print "What is your address?"
+address = gets.chomp
 
-# print "What borough do you live in?"
-# boro = gets.chomp.downcase
+print "What borough do you live in?"
+boro = gets.chomp.downcase
 
-# case boro
-# when "brooklyn"
-#   puts "How nice for you."
-# when "manhattan"
-#   puts "Well, isn't that great."
-# when "staten island"
-#   puts "Nice ferry."
-# when "queens"
-#   puts "Lets go Mets!"
-# when "bronx"
-#   puts "Yankees suck!"
-# else
-#   puts "That isn't a borough..."
-# end
+case boro
+when "brooklyn"
+  puts "How nice for you."
+when "manhattan"
+  puts "Well, isn't that great."
+when "staten island"
+  puts "Nice ferry."
+when "queens"
+  puts "Lets go Mets!"
+when "bronx"
+  puts "Yankees suck!"
+else
+  puts "That isn't a borough..."
+end
 
-# #--------------------
+#--------------------
 
-# puts "Your name is #{full_name} and you live at #{address}."
-# if canDrink == true
-#     puts "You can also have a drink at #{address}"
-# end
+puts "Your name is #{full_name} and you live at #{address}."
+if canDrink == true
+    puts "You can also have a drink at #{address}"
+end
 
-# #--------------------
+#--------------------
 
-# puts "What is 2 to the 16th power?"
-# answer = gets.chomp.to_i
+puts "What is 2 to the 16th power?"
+answer = gets.chomp.to_i
 
-# while answer != 65536
-#   puts "Try again"
-#   answer = gets.chomp.to_i
-# end
+while answer != 65536
+  puts "Try again"
+  answer = gets.chomp.to_i
+end
 
-# puts "Well done!"
+puts "Well done!"
 
-##----------------------
+#----------------------
 
 def area(length, width)
   length * width
@@ -82,7 +82,7 @@ def exponent(x, y)
   x**y
 end
 
-def name_tag_generator(first, last, gender)
+def name_tag_generator(first, last, gender, age)
 
   if gender == "f"
     if age <19
