db87996fe120a08328dc39928f87eef3c665e320
Ron Jones <rrrron@gmail.com>
2013-01-31 14:25:23 -0500

    end of lecture?

--- lectures/2013-01-31.rb
+++ lectures/2013-01-31.rb
@@ -1,23 +1,52 @@
 require "pry"
 
-class Person
+#Animal class
+
+class Animal
+  def speak
+    puts "I am an animal"
+  end
+end
+
+module A
+  def stuff1
+    puts "this is stuff1"
+  end
+  def stuff2
+    puts "this is stuff2"
+  end
+end
+
+module B
+  def temp1
+    puts "temp1"
+  end
+  def temp2
+    puts "temp2"
+  end
+end
+
+# Person class
+class Person < Animal
+  include A
+  include B
 
   attr_accessor :age, :name, :gender
 
-  def initialize(age, gender, name)
+  def to_s
+    "#{name} is a #{age} year old #{gender}"
+  end
 
+  def initialize(age, gender, name)
     @age = age
     @gender = gender
     @name = name
-
   end
 
 end
 
-def initialize(age, gender, name)
 
 
-end
 
 
 binding.pry
